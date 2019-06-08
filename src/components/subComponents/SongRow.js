11a0c10f86051b8379d0e3a6391f95a4857b7617
Marcus Quettan <mquettan@gmail.com>
2019-06-08 11:20:16 -0400

    Updating song row to promot the state up to the audio file shop for submitting.

--- src/components/subComponents/SongRow.js
+++ src/components/subComponents/SongRow.js
@@ -11,7 +11,6 @@ export default class SongRow extends Component {
     this.handleSongClick = this.handleSongClick.bind(this)
     this.handleLicenseChange = this.handleLicenseChange.bind(this)
     this.handleSelectionChange = this.handleSelectionChange.bind(this)
-    this.state = {license_tier: "Basic", selected_for_purchase: false}
   }
 
   handleSongClick() {
@@ -19,28 +18,28 @@ export default class SongRow extends Component {
   }
 
   handleLicenseChange(event) {
-    // TODO:  Lift the license and selection state up!  Pass the song name along with it.
-    this.setState({license_tier: event.target.value});
+    // Lift State
+    this.props.handleLicenseChange(event, this.props.categoryId, this.props.songId);
   }
 
   handleSelectionChange(event) {
-    // TODO:  Lift the license and selection state up!  Pass the song name along with it.
-    this.setState({selected_for_purchase: event.target.checked});
+    // Lift State
+    this.props.handleSelectionChange(event, this.props.categoryId, this.props.songId);
   }
 
   render() {
-    // TODO:  Use the props state for license and selection once state is lifted.
-    const license_tier = this.state.license_tier;
-    const isChecked = this.state.selected_for_purchase;
+    const license_tier = this.props.licenseTier;
+    const isChecked = this.props.selectedForPurchase;
     const songName = this.props.songName;
-    const uniqueId = this.props.songId.toString() + this.props.categoryId.toString();
 
     return (
       <tr id={songName} className={"song-row playing-" + this.props.isActive.toString()}>
         <th className="song-title" onClick={this.handleSongClick}>{songName}</th>
         <td className="shopping-selection">
-          <input id={"purchaseSong" + uniqueId} value="purchase" type="checkbox" onChange={this.handleSelectionChange} checked={isChecked} />
-          <label htmlFor={"purchaseSong" + uniqueId}>Select For Purchase</label>
+          <label>
+            <input value="purchase" type="checkbox" onChange={this.handleSelectionChange} checked={isChecked} />
+            Select For Purchase
+          </label>
           <select name="license-tier" onChange={this.handleLicenseChange} value={license_tier}>
             <option value="Basic">Basic</option>
             <option value="Premium">Premium</option>
