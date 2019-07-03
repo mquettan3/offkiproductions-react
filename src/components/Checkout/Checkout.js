9a4623a681b9e72a3f6e4d2ffd1c4813cac1903e
Marcus Quettan <mquettan@gmail.com>
2019-07-03 00:22:12 -0400

    Adding back in route to order confirmation page.

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -220,7 +220,7 @@ export default class Checkout extends Component {
         });
 
         console.log(response);
-        
+        this.routeToPaymentConfirmation(details.orderID)
       }.bind(this))
       .catch(function (error) {
         // handle error
