41297207f7c6820c4906fe1e1fa9705271f4e8d5
Marcus Quettan <mquettan@gmail.com>
2019-07-02 23:12:34 -0400

    Fixing paypal error messages.

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -226,7 +226,7 @@ export default class Checkout extends Component {
         console.log(error);
 
         // Navigate to the error page with the error message
-        this.onError(error.message);
+        this.onError(error.response.data);
       }.bind(this))
       .finally(function () {
         // always executed
